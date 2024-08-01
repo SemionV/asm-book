ba261eb073ad6f774e14fe227b898878ab2d71ea
SemionV <hermit.sem@gmail.com>
2024-08-01 08:33:10 +0200

    CLI exercise

---
+++ chapter21/cmdline.asm
@@ -0,0 +1,34 @@
+extern printf
+section .data
+    strArgsCount        db "CLI arguments count: %ld",0xA,0
+    strArg              db "%s",0xA,0
+section .bss
+section .text
+global main
+main:
+    push    rbp
+    mov     rbp, rsp
+
+    mov     r12, rdi
+    mov     r13, rsi
+
+    mov     rdi, strArgsCount
+    mov     rsi, r12
+    xor     rax, rax
+    call    printf
+
+    xor     r14, r14
+    .loop:
+            cmp     r14, r12
+            je      .done
+            mov     rdi, strArg
+            mov     rsi, qword [r13 + r14*8]
+            xor     rax, rax
+            call    printf
+            inc     r14
+            jmp     .loop
+
+    .done:
+
+    leave
+    ret
