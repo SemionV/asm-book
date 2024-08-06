9cbcf19a817f13420b4a2d10081d43dabbf2dad7
SemionV <hermit.sem@gmail.com>
2024-08-06 12:00:08 +0200

    sse exercise

--- chapter28/vectors.asm
+++ chapter28/vectors.asm
@@ -6,6 +6,8 @@ align 16
     vector1 dd  0.34, 0.67, 0.0, 0.913
     vector2 dd  0.1, 0.2, 0.3, 0.4
 section .bss
+alignb 16
+        vector_res resd 4
 section .text
 global  main
 main:
@@ -14,13 +16,20 @@ main:
 
     mov     rdi, vector1
     call    printVector4f
-
     mov     rdi, NL
     call    printString
 
     mov     rdi, vector2
     call    printVector4f
+    mov     rdi, NL
+    call    printString
 
+    movaps  xmm0, [vector1]
+    addps 	xmm0, [vector2]
+    movaps 	[vector_res], xmm0
+
+    mov     rdi, vector_res
+    call    printVector4f
     mov     rdi, NL
     call    printString
 
